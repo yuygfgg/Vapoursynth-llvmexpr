b42fa313518c398e6e2ac7537db6e1f3ff081487
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-09-25 23:14:55 +0800

    Add and format some liscence headers

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,3 +1,22 @@
+"""
+Copyright (C) 2025 yuygfgg
+
+This file is part of Vapoursynth-llvmexpr.
+
+Vapoursynth-llvmexpr is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vapoursynth-llvmexpr is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vapoursynth-llvmexpr.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import time
 import abc
 import sys
